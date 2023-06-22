9bd4840d98d838dd1e67fb24eae80de6586ed13c
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-06-22 23:09:45 +0200

    b

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-function Book(title, author, numberOfPages, isRead) {
-  this.title = title;
-  this.author = author;
-  this.numberOfPages = numberOfPages;
-  this.isRead = isRead;
-  this.info = function () {
-    return `${title} , ${author}, ${numberOfPages}, ${isRead}`;
-  };
+// function Book(title, author, pages, isRead) {
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.isRead = isRead;
+//   this.info = function () {
+//     return `${title} , ${author}, ${pages}, ${isRead}`;
+//     // `${title} , ${author}, ${pages}, ${isRead}`
+//   };
+// }
+
+// const bookOne = new Book("Bab", "Someone", 24, true);
+// console.log(bookOne.info());
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
 }
 
-const bookOne = new Book("Bab", "Someone", 24, true);
-console.log(bookOne.info());
+function addBookToLibrary() {
+  // do stuff here
+}
