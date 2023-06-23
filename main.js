d050aaced3f65a15aa8ae57a78a2ca3f1ff8dceb
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-06-23 22:54:41 +0200

    Made an object construcror

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
-// function Book(title, author, pages, isRead) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.isRead = isRead;
-//   this.info = function () {
-//     return `${title} , ${author}, ${pages}, ${isRead}`;
-//     // `${title} , ${author}, ${pages}, ${isRead}`
-//   };
-// }
-
 // const bookOne = new Book("Bab", "Someone", 24, true);
 // console.log(bookOne.info());
 
+const addBookBtn = document.getElementById("add");
+let booksAdded = 0;
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.info = function () {
+    return `${title} , ${author}, ${pages}, ${isRead}`;
+  };
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = prompt("What's the name of the book?");
+  let author = prompt("What's the name of an author of the book?");
+  let pages = prompt("What's the number of pages");
+  let isRead = prompt("Did you complete that book?");
+  booksAdded++;
+  const bookOne = new Book(title, author, pages, isRead);
 }
