beb69699ca7f2c063a29209ac448fbdda90285c1
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-06-24 23:07:21 +0200

    Made a method inside a book constructor

--- main.js
+++ main.js
@@ -1,44 +1,17 @@
 // const bookOne = new Book("Bab", "Someone", 24, true);
 // console.log(bookOne.info());
 const bookOne = new Book("Bab", "Someone", 24, true);
+const bookTwo = new Book("Sab", "Bomeone", 21, true);
+const bookThree = new Book("Man", "Domeone", 22, false);
 
 let booksAdded = 0;
-let myLibrary = [bookOne];
+let myLibrary = [bookOne, bookTwo, bookThree];
 
 const bookHolder = document.getElementById("books");
 const addBookBtn = document.getElementById("add");
-createBP(bookOne);
 
-// bookHolder.innerHTML = "";
 addBookBtn.addEventListener("click", addBookToLibrary);
-
-function createBP(book) {
-  //element that wraps
-  let div = document.createElement("div");
-  //child elements
-  let bookName = document.createElement("p");
-  bookName.innerText = book.title;
-  let bookAuthor = document.createElement("p");
-  bookAuthor.innerText = book.author;
-  let numberOfPages = document.createElement("p");
-  numberOfPages.innerText = book.pages;
-  let isBookRead = book.isRead;
-  let BookRead = document.createElement("p");
-  if (isBookRead == true) {
-    BookRead.innerText = "Read";
-  } else {
-    BookRead.innerText = "Not read";
-  }
-  let rmBtn = document.createElement("button");
-  rmBtn.innerText = "delete";
-
-  bookHolder.appendChild(div);
-  div.appendChild(bookName);
-  div.appendChild(bookAuthor);
-  div.appendChild(numberOfPages);
-  div.appendChild(BookRead);
-  div.appendChild(rmBtn);
-}
+myLibrary.forEach((book) => book.createBP());
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -47,6 +20,34 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
   this.info = function () {
     return `${title} , ${author}, ${pages}, ${isRead}`;
+  };
+
+  this.createBP = function () {
+    //element that wraps
+    let div = document.createElement("div");
+    //child elements
+    let bookName = document.createElement("p");
+    bookName.innerText = title;
+    let bookAuthor = document.createElement("p");
+    bookAuthor.innerText = author;
+    let numberOfPages = document.createElement("p");
+    numberOfPages.innerText = pages;
+    let isBookRead = isRead;
+    let BookRead = document.createElement("p");
+    if (isBookRead == true) {
+      BookRead.innerText = "Read";
+    } else {
+      BookRead.innerText = "Not read";
+    }
+    let rmBtn = document.createElement("button");
+    rmBtn.innerText = "delete";
+
+    bookHolder.appendChild(div);
+    div.appendChild(bookName);
+    div.appendChild(bookAuthor);
+    div.appendChild(numberOfPages);
+    div.appendChild(BookRead);
+    div.appendChild(rmBtn);
   };
 }
 
