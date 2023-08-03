6afcf2db6871d3a856e986772317073c03c3cda8
Kokonoi <91699930+Kokonoi@users.noreply.github.com>
2023-08-03 18:01:37 +0200

    made it use class for constructor

--- main.js
+++ main.js
@@ -20,55 +20,56 @@ submitBookBtn.addEventListener("submit", (event) => {
 
 myLibrary.forEach((book) => book.createBP());
 
-function Book(title, author, pages, isRead, identifier) {
-  this.title = title;
-  this.author = author;
-  this.pages = +pages;
-  this.isRead = isRead;
-  this.identifier = identifier;
+class Book {
+  constructor(title, author, pages, isRead, identifier) {
+    this.title = title;
+    this.author = author;
+    this.pages = +pages;
+    this.isRead = isRead;
+    this.identifier = identifier;
 
-  //function that creates a container with the particular elements that are needed to store a form
-  this.createBP = function (id) {
-    //element that wraps
-    let div = document.createElement("div");
-    div.classList.add(id);
-    //child elements
-    let bookName = document.createElement("p");
-    bookName.innerText = title;
-    let bookAuthor = document.createElement("p");
-    bookAuthor.innerText = author;
-    let numberOfPages = document.createElement("p");
-    numberOfPages.innerText = pages;
-    //read button
-    let BookRead = document.createElement("button");
-    if (isRead == true) {
-      BookRead.innerText = "Read";
-    } else {
-      BookRead.innerText = "Not read";
-    }
-    //
-    BookRead.addEventListener("click", () => {
-      this.isRead = !this.isRead;
-      if (this.isRead) {
+    //function that creates a container with the particular elements that are needed to store a form
+    this.createBP = function (id) {
+      //element that wraps
+      let div = document.createElement("div");
+      div.classList.add(id);
+      //child elements
+      let bookName = document.createElement("p");
+      bookName.innerText = title;
+      let bookAuthor = document.createElement("p");
+      bookAuthor.innerText = author;
+      let numberOfPages = document.createElement("p");
+      numberOfPages.innerText = pages;
+      //read button
+      let BookRead = document.createElement("button");
+      if (isRead == true) {
         BookRead.innerText = "Read";
       } else {
         BookRead.innerText = "Not read";
       }
-    });
-    //remove button
-    let rmBtn = document.createElement("button");
-    rmBtn.innerText = "delete";
-    rmBtn.addEventListener("click", () => removeBook(identifier));
-    //making html elements
-    bookHolder.appendChild(div);
-    div.appendChild(bookName);
-    div.appendChild(bookAuthor);
-    div.appendChild(numberOfPages);
-    div.appendChild(BookRead);
-    div.appendChild(rmBtn);
-  };
+      //
+      BookRead.addEventListener("click", () => {
+        this.isRead = !this.isRead;
+        if (this.isRead) {
+          BookRead.innerText = "Read";
+        } else {
+          BookRead.innerText = "Not read";
+        }
+      });
+      //remove button
+      let rmBtn = document.createElement("button");
+      rmBtn.innerText = "delete";
+      rmBtn.addEventListener("click", () => removeBook(identifier));
+      //making html elements
+      bookHolder.appendChild(div);
+      div.appendChild(bookName);
+      div.appendChild(bookAuthor);
+      div.appendChild(numberOfPages);
+      div.appendChild(BookRead);
+      div.appendChild(rmBtn);
+    };
+  }
 }
-
 function displayForm() {
   formContainer.style.display = "block";
 }
